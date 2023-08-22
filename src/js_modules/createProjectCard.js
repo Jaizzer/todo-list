d4fc7c6d6490f5205ab1452eee2456c77e1405c7
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-22 17:46:55 +0800

    feat: Add function that adds a delete button to the project card

--- src/js_modules/createProjectCard.js
+++ src/js_modules/createProjectCard.js
@@ -16,3 +16,31 @@ export function createProjectCard(project) {
 
     return projectCard;
 }
+
+
+function addDeleteButton(projectCard) {
+
+    // Create delete button.
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "delete";
+
+    // Create the delete svg icon.
+    deleteButton.innerHTML = `<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <title>Delete</title>
+                                <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,
+                                8H11V17H9V8M13,8H15V17H13V8Z"/>
+                            </svg>`;
+
+    // Add delete functionality to delete button.
+    deleteButton.addEventListener("click", () => {
+        
+        // Delete Project (back-end)
+        projectCard.projectReference.delete();
+
+        // Delete Project (front-end)
+        projectCard.parentNode.removeChild(projectCard);
+
+    })
+
+    projectCard.appendChild(deleteButton);
+}
