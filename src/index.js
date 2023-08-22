dc5adf823389705470e929b7ececf54f08ed916b
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-22 17:41:42 +0800

    feat: Implement initial project adding feature when the 'Add Project' button is pressed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Project } from "./js_modules/Project.js";
 import { createProjectCard } from "./js_modules/createProjectCard.js";
+import { createProjectAdderForm } from "./js_modules/createProjectAdderForm.js";
 import "./style.css";
 
 window.addEventListener("load", () => {
@@ -12,4 +13,11 @@ window.addEventListener("load", () => {
     Project.projects.forEach(project => {
         projectsTab.appendChild(createProjectCard(project));
     })
+})
+
+// Load project adder form when "Add Project" button is pressed.
+const projectAdderButton = document.querySelector(".project-adder");
+projectAdderButton.addEventListener("click", () => {
+    const projectAdderForm = createProjectAdderForm();
+    document.body.appendChild(projectAdderForm);
 })
