e361159d897f4d3580b93b0e47806a83fa8ee80f
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-22 17:41:04 +0800

    feat: Implement initial project rendering feature when the page loads

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import { Project } from "./js_modules/Project.js";
+import { createProjectCard } from "./js_modules/createProjectCard.js";
 import "./style.css";
+
+window.addEventListener("load", () => {
+
+    // Load all saved projects.
+    Project.loadProjects();
+
+    // Load all projects to projects-tab.
+    const projectsTab = document.querySelector(".projects-tab")
+    Project.projects.forEach(project => {
+        projectsTab.appendChild(createProjectCard(project));
+    })
+})
