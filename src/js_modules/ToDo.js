bdc70f84036bc9d898c972d446ab59555cba2e22
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-22 14:11:30 +0800

    feat: Add module that contains the class representing a ToDo item

---
+++ src/js_modules/ToDo.js
@@ -0,0 +1,18 @@
+/**
+ * @class
+ * @classdesc Represents a to-do item with various details.
+ * @param {string} title - The title or name of the to-do item.
+ * @param {string} description - A detailed description of the to-do item.
+ * @param {Date} dueDate - The due date for completing the to-do item.
+ * @param {string} priority - The priority level of the to-do item (e.g., "High", "Medium", "Low").
+ * @param {string} notes - Additional notes or comments related to the to-do item.
+ */
+export class ToDo {
+    constructor(title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+}
