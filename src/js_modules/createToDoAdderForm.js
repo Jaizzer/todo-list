41f8bcb774c0b5fadf3eb9fd169cdb37289e790d
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-23 20:17:20 +0800

    feat: Add feature that creates title input in form

--- src/js_modules/createToDoAdderForm.js
+++ src/js_modules/createToDoAdderForm.js
@@ -3,5 +3,12 @@ export function createToDoAdderForm() {
     const toDoAdderForm = document.createElement("form");
     toDoAdderForm.className = "to-do-adder-form";
 
+    // Create title input.
+    const titleInput = document.createElement("input");
+    titleInput.type = "text";
+    titleInput.id = "title";
+    titleInput.name = "title";
+    titleInput.required = true;
+
     return toDoAdderForm;
 }
