98e85af98f506ccf4fdb1c8945a06b6aa5ceba66
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-23 20:12:33 +0800

    feat: Add module that creates a form that allow user to create ToDo items

---
+++ src/js_modules/createToDoAdderForm.js
@@ -0,0 +1,7 @@
+export function createToDoAdderForm() {
+
+    const toDoAdderForm = document.createElement("form");
+    toDoAdderForm.className = "to-do-adder-form";
+
+    return toDoAdderForm;
+}
