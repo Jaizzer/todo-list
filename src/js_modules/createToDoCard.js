4d0d6c14de91248838e00a4eb83c4f24902d702d
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-24 21:47:24 +0800

    feat: Add module that returns a to-do item card

---
+++ src/js_modules/createToDoCard.js
@@ -0,0 +1,20 @@
+export function createToDoCard(ToDo) {
+
+    // Create to-do card div.
+    const toDoCard = document.createElement("div");
+    toDoCard.className = "to-do-container"
+    
+    // Create to-do title container.
+    const toDoTitle = document.createElement("div");
+    toDoTitle.className = "to-do-title"
+    toDoTitle.textContent = ToDo.title;
+    toDoCard.appendChild(toDoTitle);
+
+    // Create to-do due date container.
+    const toDoDueDate = document.createElement("div");
+    toDoDueDate.className = "to-do-due-date";
+    toDoDueDate.textContent = ToDo.dueDate;
+    toDoCard.appendChild(toDoDueDate);
+
+    return toDoCard;
+}
