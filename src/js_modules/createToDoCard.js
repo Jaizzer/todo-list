38e9b3b3bde374bb57f6fea35a2367a0e33950ef
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-25 22:09:26 +0800

    feat: Add utility function for adding delete button to a to-do card

--- src/js_modules/createToDoCard.js
+++ src/js_modules/createToDoCard.js
@@ -21,3 +21,31 @@ export function createToDoCard(ToDo) {
 
     return toDoCard;
 }
+
+
+function addDeleteButton(toDoCard) {
+
+    // Create delete button.
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "delete";
+
+    // Create the delete svg icon.
+    deleteButton.innerHTML = `<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <title>Delete</title>
+                                <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,
+                                8H11V17H9V8M13,8H15V17H13V8Z"/>
+                            </svg>`;
+
+    // Add delete functionality to delete button.
+    deleteButton.addEventListener("click", () => {
+        
+        // Delete Project (back-end)
+        toDoCard.toDoReference.delete();
+
+        // Delete Project (front-end)
+        toDoCard.parentNode.removeChild(toDoCard);
+
+    })
+
+    toDoCard.appendChild(deleteButton);
+}
