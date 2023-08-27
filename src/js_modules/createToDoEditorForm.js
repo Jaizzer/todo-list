4cd8aaf0754fab547824cb2cc5c4efba69199bc6
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-27 23:17:13 +0800

    feat: Rename module

---
+++ src/js_modules/createToDoEditorForm.js
@@ -0,0 +1,116 @@
+import { createToDoCard } from "./createToDoCard";
+import { ToDo } from "./ToDo";
+
+export function createToDoEditorForm(toDoCard) {
+
+    const toDoEditorForm = document.createElement("form");
+    toDoEditorForm.className = "to-do-adder-form";
+
+    // Create title input.
+    const titleInput = document.createElement("input");
+    titleInput.type = "text";
+    titleInput.id = "title";
+    titleInput.name = "title";
+    titleInput.required = true;
+    titleInput.value = toDoCard.toDoReference.title;
+    toDoEditorForm.appendChild(createFieldDiv("Title:", titleInput));
+
+    // Create description area.
+    const descriptionTextarea = document.createElement("textarea");
+    descriptionTextarea.id = "description";
+    descriptionTextarea.name = "description";
+    descriptionTextarea.rows = 4;
+    descriptionTextarea.cols = 50;
+    descriptionTextarea.required = true;
+    descriptionTextarea.value = toDoCard.toDoReference.description;
+    toDoEditorForm.appendChild(createFieldDiv("Description:", descriptionTextarea));
+
+    // Create due date input.
+    const dueDateInput = document.createElement("input");
+    dueDateInput.type = "date";
+    dueDateInput.id = "due-date";
+    dueDateInput.name = "dueDate";
+    dueDateInput.required = true;
+    dueDateInput.value = toDoCard.toDoReference.dueDate;
+    toDoEditorForm.appendChild(createFieldDiv("Due Date:", dueDateInput));
+
+    // Create ToDo priority selection.
+    const prioritySelect = document.createElement("select");
+    prioritySelect.id = "priority";
+    prioritySelect.name = "priority";
+
+    const priorityOptions = ["High", "Medium", "Low"];
+    priorityOptions.forEach(optionValue => {
+        const option = document.createElement("option");
+        option.value = optionValue;
+        option.textContent = optionValue;
+        prioritySelect.appendChild(option);
+    });
+    prioritySelect.value = toDoCard.toDoReference.priority;
+    toDoEditorForm.appendChild(createFieldDiv("Priority:", prioritySelect));
+
+    // Create Note input.
+    const notesTextarea = document.createElement("textarea");
+    notesTextarea.id = "notes";
+    notesTextarea.name = "notes";
+    notesTextarea.rows = 4;
+    notesTextarea.cols = 50;
+    notesTextarea.value = toDoCard.toDoReference.notes;
+    toDoEditorForm.appendChild(createFieldDiv("Notes:", notesTextarea));
+
+    // Create submit button.
+    const submitButton = document.createElement("input");
+    submitButton.type = "submit";
+    submitButton.id = "submit-form";
+    submitButton.value = "Save Changes";
+    toDoEditorForm.appendChild(submitButton);
+
+    // Create cancel button.
+    const cancelButton = document.createElement("input");
+    cancelButton.type = "button";
+    cancelButton.value = "Cancel";
+    cancelButton.addEventListener("click", () => {
+        toDoEditorForm.parentElement.removeChild(toDoEditorForm);
+    })
+    toDoEditorForm.appendChild(cancelButton);
+
+    // Add feature to form that when submitted, a ToDo object is created.
+    toDoEditorForm.addEventListener("submit", event => {
+
+        // Prevent form submission.
+        event.preventDefault()
+
+        // Parse form control values.
+        const toDoTitle = document.querySelector("#title").value;
+        const toDoDescription = document.querySelector("#description").value;
+        const toDoDueDate = document.querySelector("#due-date").value;
+        const toDoPriority = document.querySelector("#priority").value;
+        const toDoNotes = document.querySelector("#notes").value;
+
+        // Create a ToDo item with all changes applied.
+        const updatedToDoItem = new ToDo(...[toDoTitle, toDoDescription, toDoDueDate, toDoPriority, toDoNotes])
+
+        // Replace the current ToDo item with the ToDo item with all changes applied in parent project's "toDoItems" array.
+        toDoCard.toDoReference.project.replaceToDo(updatedToDoItem, toDoCard.toDoReference);
+
+        // Update corresponding to do card in DOM.
+        const updatedToDoCard = createToDoCard(updatedToDoItem);
+        document.querySelector(".to-do-tab").replaceChild(updatedToDoCard, toDoCard);
+
+        // Remove the form from the DOM once submitted.
+        toDoEditorForm.parentElement.removeChild(toDoEditorForm);
+    });
+
+    return toDoEditorForm;
+}
+
+
+// Returns a div containing a labeled input.
+function createFieldDiv(labelText, inputElement) {
+    const div = document.createElement("div");
+    const label = document.createElement("label");
+    label.textContent = labelText;
+    label.appendChild(inputElement);
+    div.appendChild(label);
+    return div;
+}
