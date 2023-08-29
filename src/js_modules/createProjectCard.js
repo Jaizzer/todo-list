a2840653bf1b2b2a91eb629ee2316c58b534ad82
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-29 13:27:10 +0800

    feat: Add feature that puts home icon to 'home' project card

--- src/js_modules/createProjectCard.js
+++ src/js_modules/createProjectCard.js
@@ -12,6 +12,14 @@ export function createProjectCard(project) {
     // Add referece to the project object.
     projectCard.projectReference = project;
 
+    // Add home icon to home-project.
+    if (project.projectId === "home-project") {
+        const homeIcon = document.createElement("div");
+        homeIcon.className = "icon-container"
+        homeIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24"><title>home</title><path d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" /></svg>`;
+        projectCard.appendChild(homeIcon);
+    }
+
     // Put project title inside the project card.
     const projectTitle = document.createElement("div");
     projectTitle.className = "project-name"
