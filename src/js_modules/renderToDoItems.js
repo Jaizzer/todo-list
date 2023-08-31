d296bad3131a3026a1ec992c0eb70a0ba78dcc78
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-31 09:28:37 +0800

    feat: Incorporate sorting in to do card rendering module

--- src/js_modules/renderToDoItems.js
+++ src/js_modules/renderToDoItems.js
@@ -1,5 +1,6 @@
 import { createToDoCard } from "./createToDoCard";
 import { Project } from "./Project";
+import { sort } from "./sort";
 
 export function renderToDoItems(currentProject) {
 
@@ -9,17 +10,28 @@ export function renderToDoItems(currentProject) {
     // Clear previous to-do container contents.
     toDoTab.innerHTML = "";
 
+    // Array for storing all unsorted ToDo items.
+    let unsortedToDoItems = [];
+
     // Render all ToDos across all projects if the project is "Home"
     if (currentProject === Project.home) {
         Project.projects.forEach(project => project.toDoItems
             .forEach(toDoItem => {
-                toDoTab.appendChild(createToDoCard(toDoItem));
+                unsortedToDoItems.push(toDoItem);
             }));
     }
     // If current project is not "Home", only render its respective ToDo items.
     else {
         currentProject.toDoItems.forEach(toDoItem => {
-            toDoTab.appendChild(createToDoCard(toDoItem));
+            unsortedToDoItems.push(toDoItem);
         });
     }
+
+    // Sort.
+    const sortedToDoItems = sort(unsortedToDoItems);
+
+    // Render all ToDo cards to ToDo tab.
+    sortedToDoItems.forEach(toDoItem => {
+        document.querySelector(".to-do-tab").appendChild(createToDoCard(toDoItem));
+    })
 }
