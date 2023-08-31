e1d7dce85f79e235df8384d1b89b9ae28903a8ce
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-31 08:58:16 +0800

    feat: Add descending and ascending option when sorting

--- src/js_modules/sortBy.js
+++ src/js_modules/sortBy.js
@@ -13,7 +13,7 @@ export function sortBy(sortCriteria) {
     let sortedItems;
 
     // Sort by due date.
-    if (sortCriteria === "date") {
+    if (sortCriteria === "date-ascending") {
         sortedItems = itemsToBeSorted.sort((ToDoCard1, ToDoCard2) => {
 
             const ToDo1Date = new Date(ToDoCard1.toDoReference.dueDate);
@@ -29,10 +29,41 @@ export function sortBy(sortCriteria) {
             toDoTab.appendChild(item);
         })
     }
-    // Sort by name.
-    else if (sortCriteria === "name") {
+    // Sort by due date date
+    else if (sortCriteria === "date-descending") {
+        sortedItems = itemsToBeSorted.sort((ToDoCard1, ToDoCard2) => {
+
+            const ToDo1Date = new Date(ToDoCard1.toDoReference.dueDate);
+            const ToDo2Date = new Date(ToDoCard2.toDoReference.dueDate);
+            return ToDo2Date - ToDo1Date;
+        })
+
+        // Clear previous to-do container contents.
+        toDoTab.innerHTML = "";
+
+        // Put the sorted to-do cards.
+        sortedItems.forEach(item => {
+            toDoTab.appendChild(item);
+        })
+    }
+    // Sort by name ascendingly.
+    else if (sortCriteria === "name-ascending") {
         sortedItems = itemsToBeSorted.sort((ToDoCard1, ToDoCard2) => {
             return ToDoCard1.toDoReference.title.localeCompare(ToDoCard2.toDoReference.title);
+        })
+
+        // Clear previous to-do container contents.
+        toDoTab.innerHTML = "";
+
+        // Put the sorted to-do cards.
+        sortedItems.forEach(item => {
+            toDoTab.appendChild(item);
+        })
+    }
+    // Sort by name descendingly.
+    else if (sortCriteria === "name-descending") {
+        sortedItems = itemsToBeSorted.sort((ToDoCard1, ToDoCard2) => {
+            return ToDoCard2.toDoReference.title.localeCompare(ToDoCard1.toDoReference.title);
         })
 
         // Clear previous to-do container contents.
