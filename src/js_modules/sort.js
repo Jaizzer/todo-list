16bba57e94e83f319ebd7af2599b3ac04695de5e
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-31 12:58:31 +0800

    doc: Update comments in sort module

--- src/js_modules/sort.js
+++ src/js_modules/sort.js
@@ -1,12 +1,17 @@
+/**
+ * Sorts an array of to-do items based on the selected sorting criteria.
+ *
+ * @param {Array} unsortedToDoItems - An array of to-do items to be sorted.
+ * @returns {Array}  An array of sorted to-do items.
+ */
 export function sort(unsortedToDoItems) {
-
-    // Sorted items.
+    // Array to hold sorted items.
     let sortedToDoItems;
 
-    // Get the sort critera.
+    // Get the sorting criteria selected by the user.
     const sortCriteria = document.querySelector("#sorter").value;
-    
-    // Sort by due date.
+
+    // Sort by due date in ascending order.
     if (sortCriteria === "date-ascending") {
         sortedToDoItems = unsortedToDoItems.sort((ToDo1, ToDo2) => {
             const ToDo1Date = new Date(ToDo1.dueDate);
@@ -14,28 +19,27 @@ export function sort(unsortedToDoItems) {
             return ToDo1Date - ToDo2Date;
         });
     }
-    // Sort by due date date
+    // Sort by due date in descending order.
     else if (sortCriteria === "date-descending") {
         sortedToDoItems = unsortedToDoItems.sort((ToDo1, ToDo2) => {
-
             const ToDo1Date = new Date(ToDo1.dueDate);
             const ToDo2Date = new Date(ToDo2.dueDate);
             return ToDo2Date - ToDo1Date;
         });
     }
-    // Sort by name ascendingly.
+    // Sort by name in ascending order.
     else if (sortCriteria === "name-ascending") {
         sortedToDoItems = unsortedToDoItems.sort((ToDo1, ToDo2) => {
             return ToDo1.title.localeCompare(ToDo2.title);
         });
     }
-    // Sort by name descendingly.
+    // Sort by name in descending order.
     else if (sortCriteria === "name-descending") {
         sortedToDoItems = unsortedToDoItems.sort((ToDo1, ToDo2) => {
             return ToDo2.title.localeCompare(ToDo1.title);
         });
     }
-    //Sort by time of creation (oldest to newest)
+    // Sort by time of creation (oldest to newest).
     else if (sortCriteria === "time-created-ascending") {
         sortedToDoItems = unsortedToDoItems.sort((ToDo1, ToDo2) => {
             const ToDo1Date = new Date(ToDo1.dateAndTimeCreated);
@@ -43,7 +47,7 @@ export function sort(unsortedToDoItems) {
             return ToDo1Date - ToDo2Date;
         });
     }
-    //Sort by time of creation (newest to oldest.)
+    // Sort by time of creation (newest to oldest).
     else if (sortCriteria === "time-created-descending") {
         sortedToDoItems = unsortedToDoItems.sort((ToDo1, ToDo2) => {
             const ToDo1Date = new Date(ToDo1.dateAndTimeCreated);
@@ -52,6 +56,6 @@ export function sort(unsortedToDoItems) {
         });
     }
 
-
+    // Return the array of sorted to-do items.
     return sortedToDoItems;
 }
