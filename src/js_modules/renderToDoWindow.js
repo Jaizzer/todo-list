d774c887f57b8aac054e2c1279b343c567b1f81c
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-09-01 21:04:47 +0800

    feat: Add feature that creates a sorter when rendering to-do window

--- src/js_modules/renderToDoWindow.js
+++ src/js_modules/renderToDoWindow.js
@@ -5,6 +5,9 @@ export function renderToDoWindow(project) {
 
     // Add the current parent project card to the to-do window.
     toDoWindow.appendChild(createParentProjectNameCard(project));
+
+    // Add the sorter to the to-do window.
+    toDoWindow.appendChild(createSorter());
 }
 
 
@@ -14,4 +17,52 @@ function createParentProjectNameCard(project) {
     parentProjectNameCard.textContent = project.projectTitle;
     
     return parentProjectNameCard;
+}
+
+function createSorter() {
+    
+    // Create div to contain the sorter.
+    const sorterContainer = document.querySelector("div");
+    sorterContainer.className = "sorter-container"
+
+    // Create a select element that will serve as a way to choose sorting criteria.
+    const sorter = document.createElement("select");
+    sorter.id = "#sorter";
+    sorterContainer.appendChild(sorter);
+
+    // Create the label for the sorter.
+    const label = document.querySelector("label");
+    label.setAttribute("for", sorter.id);
+    label.textContent = "Sort by:";
+
+    // Create the data values of options to be selected.
+    const sortingOptions = [
+        { value: "time-created-ascending", text: "Created (Oldest to Newest)", selected: true },
+        { value: "time-created-descending", text: "Created (Newest to Oldest)" },
+        { value: "date-ascending", text: "Date (ascending)" },
+        { value: "date-descending", text: "Date (descending)" },
+        { value: "name-ascending", text: "Name (ascending)" },
+        { value: "name-descending", text: "Name (descending)" }
+    ]
+
+    sortingOptions.forEach(sortingOption => {
+        // Create option element.
+        const option = document.createElement("option");
+
+        // Add option value.
+        option.setAttribute("value", sortingOption.value);
+
+        // Add option text-content.
+        option.textContent = sortingOption.text;
+
+        // Condition to set the default selected option.
+        if (optionData.selected) {
+            option.setAttribute("selected", "selected");
+        }
+
+        // Append the option to sorter select element.
+        sorter.appendChild(option);
+    })
+
+    return sorter;
 }
