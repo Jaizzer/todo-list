d5ae2c8d8b6419eb5c54f4d4e527c0e76ddf4058
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-09-01 20:52:40 +0800

    feat: Add feature that creates a card containing the name of the current parent project selected

--- src/js_modules/renderToDoWindow.js
+++ src/js_modules/renderToDoWindow.js
@@ -2,4 +2,16 @@
 
 export function renderToDoWindow(project) {
     const toDoWindow = document.querySelector(".to-do-window");
+
+    // Add the current parent project card to the to-do window.
+    toDoWindow.appendChild(createParentProjectNameCard(project));
+}
+
+
+// Create a card containing the name of the current parent project.
+function createParentProjectNameCard(project) {
+    const parentProjectNameCard = document.createElement("div");
+    parentProjectNameCard.textContent = project.projectTitle;
+    
+    return parentProjectNameCard;
 }
