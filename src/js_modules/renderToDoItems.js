07e1ed8907000196a6efd3afed4469012e4b4450
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-09-02 17:33:46 +0800

    refactor: Remove unnecessary 'renderItems' module

--- src/js_modules/renderToDoItems.js
+++
@@ -1,9 +0,0 @@
-import { createToDoCard } from "./createToDoCard";
-import { Project } from "./Project";
-import { sort } from "./sort";
-
-export function renderToDoItems(currentProject) {
-
-    // Access to-do container.
-    const toDoTab = document.querySelector(".to-do-tab");
-}
