032959178a650d801866a5e540ad8163526c9cc1
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-09-08 17:35:02 +0800

    refactor: Refactor index module

--- src/index.js
+++ src/index.js
@@ -1,64 +1,58 @@
 import { Project } from "./js_modules/Project.js";
 import { createProjectCard } from "./js_modules/createProjectCard.js";
 import { setCurrentlySelectedProject } from "./js_modules/selectedProjectManager.js";
-
-import "./style.css";
 import { popUpProjectAdderForm } from "./js_modules/popUpProjectAdderForm.js";
+import "./style.css";
 
 window.addEventListener("load", () => {
-
-    // Load all saved projects.
     Project.loadProjects();
-
-    // Set currently selected project to "Home" when page loads.
     setCurrentlySelectedProject(Project.home);
+    loadProjectsToTabs();
+    setUpProjectAdder();
+    setUpProjectDropdown();
+});
 
-    // Load all projects to projects-tab.
-    const projectsTab = document.querySelector(".projects-tab")
-
-    // Acces "default-projects" container.
+function loadProjectsToTabs() {
+    const projectsTab = document.querySelector(".projects-tab");
     const defaultProjectsContainer = projectsTab.querySelector(".default-projects-container");
-
-    // Access "created-projects" container.
     const createdProjectsContainer = projectsTab.querySelector(".created-projects-container");
 
     Project.projects.forEach(project => {
+        const projectCard = createProjectCard(project);
 
-        const projectCard = createProjectCard(project)
-
-        if (project.id === "home-project" || project.id === "completed-project" || project.id === "today-project" || project.id === "this-week-project") {
+        if (isDefaultProject(project)) {
             defaultProjectsContainer.appendChild(projectCard);
+        } else {
+            insertProjectCard(createdProjectsContainer, projectCard);
         }
-        else {
-            // Insert project card inside crated projects container before the "project-adder" button.
-            createdProjectsContainer.insertBefore(projectCard, createdProjectsContainer.querySelector(".project-adder"));
-        }
-    })
-})
+    });
+}
 
-// Load project adder form when "Add Project" button is pressed.
-const projectAdderButton = document.querySelector(".project-adder");
-projectAdderButton.addEventListener("click", () => {
-    popUpProjectAdderForm();
-})
+function isDefaultProject(project) {
+    return (
+        project.id === "home-project" ||
+        project.id === "completed-project" ||
+        project.id === "today-project" ||
+        project.id === "this-week-project"
+    );
+}
 
+function insertProjectCard(createdProjectsContainer, projectCard) {
+    createdProjectsContainer.insertBefore(projectCard, createdProjectsContainer.querySelector(".project-adder"));
+}
 
-// Add feature to the Project's drop down button in dashboard.
-const projectDropdownButton = document.querySelector("#created-projects-dropdown");
-projectDropdownButton.addEventListener("click", () => {
+function setUpProjectAdder() {
+    const projectAdderButton = document.querySelector(".project-adder");
+    projectAdderButton.addEventListener("click", () => {
+        popUpProjectAdderForm();
+    });
+}
 
-    // Access created projects container.
-    const createdProjectsContainer = document.querySelector(".created-projects-container");
-
-    // Get all created project container's classes.
-    const classes = createdProjectsContainer.className.split(' ');
-
-    // If drop down button is clicked when created projects container is "active", make it "inactive".
-    if (classes.includes("active")) {
-        createdProjectsContainer.classList.replace("active", "inactive");
-    }
-    // If drop down button is clicked when created projects container is "inactive", make it "active".
-    else if (classes.includes("inactive")) {
-        createdProjectsContainer.classList.replace("inactive", "active");
-    }
-})
+function setUpProjectDropdown() {
+    const projectDropdownButton = document.querySelector("#created-projects-dropdown");
+    projectDropdownButton.addEventListener("click", () => {
+        const createdProjectsContainer = document.querySelector(".created-projects-container");
+        createdProjectsContainer.classList.toggle("inactive");
+        createdProjectsContainer.classList.toggle("active");
+    });
+}
